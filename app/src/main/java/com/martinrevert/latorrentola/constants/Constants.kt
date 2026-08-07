3a093e6e7083c36080f5ce8ded057402db694165
Martin Revert <martinrevert@gmail.com>
2026-08-07 12:15:50 -0300

    - Update `YTS_BASE_URL` in `Constants.kt` to point to a new API endpoint (`https://movies-api.accel.li/api/v2`) - Add `ic_launcher_banner.png` resource file

--- app/src/main/java/com/martinrevert/latorrentola/constants/Constants.kt
+++ app/src/main/java/com/martinrevert/latorrentola/constants/Constants.kt
@@ -1,7 +1,7 @@
 package com.martinrevert.latorrentola.constants
 
 object Constants {
-    const val YTS_BASE_URL = "https://yts.ag/api/v2/"
+    const val YTS_BASE_URL = "https://movies-api.accel.li/api/v2"
     const val FCM_BASE_URL = "https://fcm.martinrevert.com.ar/"
     const val PAGE_SIZE = 50
 }
